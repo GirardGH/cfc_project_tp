37573430380d3decd0655baa4c5e1f7ee8924328
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-07-01 16:07:35 +0200

    modif header

---
+++ src/components/header/Ad.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Ad () {
+  return (
+    <div>
+      <p className="bg-black h-10 flex items-center justify-center text-sm font-medium text-white px-4 sm:px-6 lg:px-8">
+        Get free delivery on orders over $100
+      </p>
+    </div>
+  );
+}
--- src/components/header/UserMenu.js
+++ src/components/header/UserMenu.js
@@ -1,65 +1,83 @@
-import { PhoneIcon } from '@heroicons/react/24/solid'
-import { AiOutlineMail } from 'react-icons/ai'
+import { PhoneIcon } from "@heroicons/react/24/solid";
+import { AiOutlineMail } from "react-icons/ai";
 
 const people = [
   {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
+    name: "Jane Cooper",
+    title: "Paradigm Representative",
+    role: "Admin",
+    email: "janecooper@example.com",
+    telephone: "+1-202-555-0170",
     imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
   },
   // More people...
-]
+];
 
 export default function UserMenu() {
   return (
-    <div className='flex justify-end'>
-    <div className="grid grid-cols-1">
-      <div
-        key={people.email}
-        className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
-      >
-        <div className="flex-1 flex flex-col p-8">
-          <img className="w-32 h-32 flex-shrink-0 mx-auto rounded-full" src={people[0].imageUrl} alt="" />
-          <h3 className="mt-6 text-gray-900 text-sm font-medium">{people[0].name}</h3>
-          <dl className="mt-1 flex-grow flex flex-col justify-between">
-            <dt className="sr-only">Title</dt>
-            <dd className="text-gray-500 text-sm">{people[0].title}</dd>
-            <dt className="sr-only">Role</dt>
-            <dd className="mt-3">
-              <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                {people[0].role}
-              </span>
-            </dd>
-          </dl>
-        </div>
-        <div>
-          <div className="-mt-px flex divide-x divide-gray-200">
-            <div className="w-0 flex-1 flex">
-              <a
-                href={`mailto:${people[0].email}`}
-                className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
-              >
-                <AiOutlineMail className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                <span className="ml-3">Email</span>
-              </a>
-            </div>
-            <div className="-ml-px w-0 flex-1 flex">
-              <a
-                href={`tel:${people[0].telephone}`}
-                className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
-              >
-                <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                <span className="ml-3">Call</span>
-              </a>
+    <div className="flex justify-end absolute top-24 right-4">
+      <div className="grid grid-cols-1">
+        <div
+          key={people.email}
+          className="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+        >
+          <div className="flex-1 flex flex-col p-8">
+            <img
+              className="w-32 h-32 flex-shrink-0 mx-auto rounded-full"
+              src={people[0].imageUrl}
+              alt=""
+            />
+            <h3 className="mt-6 text-gray-900 text-sm font-medium">
+              {people[0].name}
+            </h3>
+            <dl className="mt-1 flex-grow flex flex-col justify-between">
+              <dt className="sr-only">Title</dt>
+              <dd className="text-gray-500 text-sm">{people[0].title}</dd>
+              <dt className="sr-only">Role</dt>
+              <dd className="mt-3">
+                <span className="px-2 py-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                  {people[0].role}
+                </span>
+              </dd>
+            </dl>
+          </div>
+          <div>
+            <div className="-mt-px flex divide-x divide-gray-200">
+              <div className="w-0 flex-1 flex">
+
+                <button
+                  type="button"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >ihkjhkhjkhk
+                  {/* <a
+                    href={`mailto:${people[0].email}`}
+                    className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
+                  >
+                    <AiOutlineMail
+                      className="w-5 h-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-3">Email</span>
+                  </a> */}
+                </button>
+              </div>
+              <div className="-ml-px w-0 flex-1 flex">
+                <a
+                  href={`tel:${people[0].telephone}`}
+                  className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
+                >
+                  <PhoneIcon
+                    className="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <span className="ml-3">Call</span>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-  )
+  );
 }
