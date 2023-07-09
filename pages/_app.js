582edffffdc9c93c5530d2dd19439d362a62049b
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-07-09 23:49:44 +0200

    init next auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import '../src/app/globals.css'
+import "../src/app/globals.css";
 import { Provider } from "react-redux";
 import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
-
 
 let persistor = persistStore(store);
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
@@ -16,11 +16,13 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="Produits exotiques" />
         <link rel="icon" href="/cfc.svg" />
       </Head>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </SessionProvider>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Header from "../src/components/header";
 import Footer from "../src/components/footer";
+import { useSession, signIn, signOut } from "next-auth/react"
+
 
 export default function Home() {
+
+  const { data: session } = useSession()
+  
   return (
     <>
       <Header />
