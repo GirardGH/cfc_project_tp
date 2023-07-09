55667686f6bef258bbd1ec57951cc7a23d0bf77c
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-07-09 20:28:53 +0200

    add footer

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Header from '../src/components/header'
+import Header from "../src/components/header";
+import Footer from "../src/components/footer";
 
 export default function Home() {
   return (
-<Header />
-  )
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
