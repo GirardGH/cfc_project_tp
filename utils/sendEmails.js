161f50d8b62bb13781a9e37d0b6adfb0995822ec
GIRARD GHISLAIN <girard.gh@gmail.com>
2023-07-28 00:38:37 +0200

    half reset password

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -24,7 +24,7 @@ const oauth2Client = new OAuth2(
 
 //send email
 
-export const sendEmail = (to, url, txt, subject) => {
+export const sendEmail = (to, url, txt, subject, template) => {
     oauth2Client.setCredentials({
         refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
     });
@@ -47,7 +47,7 @@ export const sendEmail = (to, url, txt, subject) => {
         from: SENDER_EMAIL_ADDRESS,
         to: to,
         subject: subject,
-        html: activateEmailTemplate(to, url),
+        html: template(to, url),
     };
 
     smtpTransport.sendMail(mailOption, (err, infos) => {
